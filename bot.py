dc2063d64f62f45fcbe3d45a2bd501fdb2e69fa2
card-shop <kartikrawat6266@gmail.com>
2026-07-25 16:37:29 +0530

    Aura

--- bot.py
+++ bot.py
@@ -209,7 +209,7 @@ class InstaDownloader:
         url = f'https://www.instagram.com/reel/{shortcode}/'
     
         ydl_opts = {
-            'quiet': False,
+            'quiet': True,
             'no_warnings': True,
             'outtmpl': os.path.join(DOWNLOAD_DIR, f'{shortcode}.%(ext)s'),
             'format': 'bv*+ba/b',
@@ -889,9 +889,19 @@ async def process_download(update: Update, context: ContextTypes.DEFAULT_TYPE, u
         DOWNLOAD_DIR = os.path.join(original_dir, f"task_{unique_id}")
         os.makedirs(DOWNLOAD_DIR, exist_ok=True)
         
-        progress_task = asyncio.create_task(update_progress())
-        result = InstaDownloader.download_media(url)
-        progress_task.cancel()
+        progress_task = None
+        try:
+            is_reel = '/reel/' in url or '/tv/' in url
+            await msg.edit_text("📥 𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱𝗶𝗻𝗴 𝗩𝗶𝗱𝗲𝗼..." if is_reel else "📥 𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱𝗶𝗻𝗴 𝗣𝗵𝗼𝘁𝗼...", parse_mode="Markdown")
+    
+            # Start progress only for videos
+            if is_reel:
+                progress_task = asyncio.create_task(update_progress())
+
+            result = InstaDownloader.download_media(url)
+
+            if progress_task:
+                progress_task.cancel()
         
         DOWNLOAD_DIR = original_dir
         
