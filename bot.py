5ca12825a9295c965a7b03bbe367cc44b9717688
card-shop <kartikrawat6266@gmail.com>
2026-07-25 09:39:49 +0530

    Aura

--- bot.py
+++ bot.py
@@ -1012,7 +1012,7 @@ def main():
         try: os.remove(os.path.join(DOWNLOAD_DIR, f))
         except: pass
     
-    app = Application.builder().token(BOT_TOKEN).read_timeout(2000).write_timeout(2000).connect_timeout(2000).pool_timeout(2000).build()
+    app = Application.builder().token(BOT_TOKEN).read_timeout(4000).write_timeout(4000).connect_timeout(4000).pool_timeout(4000).build()
     
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("activate", activate_cmd))
